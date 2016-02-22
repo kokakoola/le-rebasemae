166748fd8e554ca8b0aa8e93cbb2c902a8fc29e9
mare hunt <mare.hunt@netgroup.ee>
2016-02-22 11:12:06 +0200

    simple navigation

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,15 +5,31 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     responsive_images: {
-      dev: {
-
-        files: [{
-          expand: true,
-          src: ['images/inspiration/*.{jpg,gif,png}'],
-          cwd: 'assets/',
-          dest: 'dist/'
-        }]
-      }
+        dev: {
+          options: {
+            sizes: [{
+              width: 320,
+              name: 'small'
+            }, {
+              width: 800,
+              name: 'large'
+            }]
+          },
+          files: [{
+            expand: true,
+            src: [
+              'images/kanal/**/*.{jpg,gif,png}',
+              'images/interior/**/*.{jpg,gif,png}',
+              'images/inspiration/**/*.{jpg,gif,png}',
+              'images/interior/**/*.{jpg,gif,png}',
+              'images/box/**/*.{jpg,gif,png}',
+              'images/basement/**/*.{jpg,gif,png}',
+              'images/location/**/*.{jpg,gif,png}', 
+            ],
+            cwd: 'src/',
+            dest: 'assets/'
+          }]
+        }
     }
   });
 
---
+++ README.md
@@ -0,0 +1,2 @@
+### gallery images: 
+put new images int src/images with named folder. init empty folder with the same name into assets/images. run `grunt`. yo will have two sizes of cut images.
